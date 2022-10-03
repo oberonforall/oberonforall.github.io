12e2c293f2679f540e6dca6039d0e78109152e11
amtoine <44101798+AntoineStevan@users.noreply.github.com>
2022-10-03 16:07:46 +0200

    REFACTOR: remove old build files (compiler#13)

--- foo.sh
+++
@@ -1,5 +0,0 @@
-for it in IF DO OF OR TO IN IS BY END NIL VAR DIV MOD FOR ELSE THEN TRUE TYPE CASE ELSIF FALSE ARRAY BEGIN CONST UNTIL WHILE RECORD REPEAT RETURN IMPORT MODULE POINTER PROCEDURE;
-do
-    echo "$it"
-    sed -i "s/\<\($it\)\>/<a href=\"keywords\/\1.html\">\1<\/a>/g" tmp.txt
-done
